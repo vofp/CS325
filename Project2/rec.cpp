530858926e83c56c77d27773709a98c7451f82eb
Soo-Hyun Yoo <yoos117@gmail.com>
2012-10-30 00:12:09 -0700

    Add recursive function.

---
+++ Project2/rec.cpp
@@ -0,0 +1,23 @@
+struct maxS {
+	int current;   // Current sum
+	int max;       // Overall max
+}
+
+struct maxS MaxSubarray(int array, int size) {
+	struct maxS ms;
+
+	if (size == 1) {   // Base case
+		ms.current = 0;
+		ms.max = array[0];
+	} else {   // Recurse on array excluding last element.
+		ms = MaxSubarray(array, size-1);
+	}
+
+	// Find maximum.
+	ms.current += array[size-1];
+	ms.current = (ms.current > 0) ? ms.current : 0;
+	ms.max = (ms.current > ms.max) ? ms.current : 0;
+
+	return ms;
+}
+
