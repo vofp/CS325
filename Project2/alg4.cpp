2b0b7875e857e4aec00b4bc9ecdd61bd74edd20f
Francis Vo <firefreakfrancis@gmail.com>
2012-10-30 08:26:34 -0700

    Finished tex TODO, needs recompile

--- Project2/alg4.cpp
+++ Project2/alg4.cpp
@@ -1,7 +1,6 @@
 /*
- * Enumeration 
- * Loop over each pair of indices i; j and compute the sum from k=i to j of a[k]. 
- * Keep the best sum you have found so far.
+ * Dynamic programming 
+ * Algorithm based on recursive algorithm
  */
 
 int MaxSubarray(int array[], int size){
