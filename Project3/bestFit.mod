e005830612365e5b29166b913fb9f4ea9b9923b1
Francis Vo <firefreakfrancis@gmail.com>
2012-11-04 22:50:33 -0800

    Francis failed, try again later

--- Project3/bestFit.mod
+++ Project3/bestFit.mod
@@ -3,7 +3,7 @@
 var a;
 var b;
 var c;
-
+var t;
 
 /* Objective function */
 minimize z: t;
@@ -13,26 +13,26 @@ minimize z: t;
 
 
 
-s.t. point_high_1 : a(1)+b(3)-c <= t;
-s.t. point_low_1 : a(1)+b(3)-c <= -t;
+s.t. point_high_1 : a*(1)+b*(3)-c <= t;
+s.t. point_low_1 : a*(1)+b*(3)-c <= -t;
 
-s.t. point_high_2 : a(2)+b(5)-c <= t;
-s.t. point_low_2 : a(2)+b(5)-c <= -t;
+s.t. point_high_2 : a*(2)+b*(5)-c <= t;
+s.t. point_low_2 : a*(2)+b*(5)-c <= -t;
 
-s.t. point_high_3 : a(3)+b(7)-c <= t;
-s.t. point_low_3 : a(3)+b(7)-c <= -t;
+s.t. point_high_3 : a*(3)+b*(7)-c <= t;
+s.t. point_low_3 : a*(3)+b*(7)-c <= -t;
 
-s.t. point_high_4 : a(5)+b(11)-c <= t;
-s.t. point_low_4 : a(5)+b(11)-c <= -t;
+s.t. point_high_4 : a*(5)+b*(11)-c <= t;
+s.t. point_low_4 : a*(5)+b*(11)-c <= -t;
 
-s.t. point_high_5 : a(7)+b(14)-c <= t;
-s.t. point_low_5 : a(7)+b(14)-c <= -t;
+s.t. point_high_5 : a*(7)+b*(14)-c <= t;
+s.t. point_low_5 : a*(7)+b*(14)-c <= -t;
 
-s.t. point_high_6 : a(8)+b(15)-c <= t;
-s.t. point_low_6 : a(8)+b(15)-c <= -t;
+s.t. point_high_6 : a*(8)+b*(15)-c <= t;
+s.t. point_low_6 : a*(8)+b*(15)-c <= -t;
 
-s.t. point_high_7 : a(10)+b(19)-c <= t;
-s.t. point_low_7 : a(10)+b(19)-c <= -t;
+s.t. point_high_7 : a*(10)+b*(19)-c <= t;
+s.t. point_low_7 : a*(10)+b*(19)-c <= -t;
 
 
 end;
---
+++ Project3/bestFit.sol
@@ -0,0 +1,51 @@
+Problem:    bestFit
+Rows:       15
+Columns:    4
+Non-zeros:  57
+Status:     UNDEFINED
+Objective:  z = 0 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 z            B              0                             
+     2 point_high_1 B              0                          -0 
+     3 point_low_1  B              0                          -0 
+     4 point_high_2 B              0                          -0 
+     5 point_low_2  B              0                          -0 
+     6 point_high_3 B              0                          -0 
+     7 point_low_3  B              0                          -0 
+     8 point_high_4 B              0                          -0 
+     9 point_low_4  B              0                          -0 
+    10 point_high_5 B              0                          -0 
+    11 point_low_5  B              0                          -0 
+    12 point_high_6 B              0                          -0 
+    13 point_low_6  B              0                          -0 
+    14 point_high_7 B              0                          -0 
+    15 point_low_7  B              0                          -0 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 a            NF             0                                     < eps
+     2 b            NF             0                                     < eps
+     3 c            NF             0                                     < eps
+     4 t            NF             0                                     < eps
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 1.00e+00 on column 4
+        max.rel.err = 5.00e-01 on column 4
+        DUAL SOLUTION IS WRONG
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
