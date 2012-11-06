fd7182f89bc0d6e6495b4f24d130b8c256531432
Francis Vo <firefreakfrancis@gmail.com>
2012-11-05 19:45:29 -0800

    remove out of data files

--- Project3/bestFit.mod
+++ Project3/bestFit.mod
@@ -1,8 +1,8 @@
 /* Decision variables */
 
-var a >= 0;
-var b >= 0;
-var c >= 0;
+var a;
+var b;
+var c;
 var t;
 
 /* Objective function */
@@ -11,11 +11,26 @@ minimize z: t;
 
 /* Constraints */
 
+s.t. point_x_high_1 : 1 - b <= t;
+s.t. point_x_low_1 : 1 - b >= -t;
 
-s.t. slope_x_high : (10-1) - b <= t;
-s.t. slope_x_low : (10-1) - b >= -t;
-s.t. slope_y_high : (19-3) - a <= t;
-s.t. slope_y_low : (19-3) - a >= -t;
+s.t. point_x_high_2 : 2 - b <= t;
+s.t. point_x_low_2 : 2 - b >= -t;
+
+s.t. point_x_high_3 : 3 - b <= t;
+s.t. point_x_low_3 : 3 - b >= -t;
+
+s.t. point_x_high_4 : 5 - b <= t;
+s.t. point_x_low_4 : 5 - b >= -t;
+
+s.t. point_x_high_5 : 7 - b <= t;
+s.t. point_x_low_5 : 7 - b >= -t;
+
+s.t. point_x_high_6 : 8 - b <= t;
+s.t. point_x_low_6 : 8 - b >= -t;
+
+s.t. point_x_high_7 : 10- b <= t;
+s.t. point_x_low_7 : 10- b >= -t;
 
 s.t. point_high_1 : a*(1)+b*(3)-c <= t;
 s.t. point_low_1 : a*(1)+b*(3)-c >= -t;
@@ -37,7 +52,5 @@ s.t. point_low_6 : a*(8)+b*(15)-c >= -t;
 
 s.t. point_high_7 : a*(10)+b*(19)-c <= t;
 s.t. point_low_7 : a*(10)+b*(19)-c >= -t;
-
-
 
 end;
--- Project3/bestFit.sol
+++ Project3/bestFit.sol
@@ -1,43 +1,67 @@
-Problem:    bestFit
-Rows:       19
+Problem:    bestFit2
+Rows:       29
 Columns:    4
-Non-zeros:  65
+Non-zeros:  85
 Status:     OPTIMAL
-Objective:  z = 13.09090909 (MINimum)
+Objective:  z = 4.5 (MINimum)
 
    No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
 ------ ------------ -- ------------- ------------- ------------- -------------
-     1 z            B        13.0909                             
-     2 slope_x_high B       -13.0909                          -9 
-     3 slope_x_low  B        13.0909            -9               
-     4 slope_y_high NU           -16                         -16     -0.818182 
-     5 slope_y_low  B        10.1818           -16               
-     6 point_high_1 B       -26.1818                          -0 
-     7 point_low_1  NL             0            -0                   0.0909091 
-     8 point_high_2 B       -23.2727                          -0 
-     9 point_low_2  B        2.90909            -0               
-    10 point_high_3 B       -20.3636                          -0 
-    11 point_low_3  B        5.81818            -0               
-    12 point_high_4 B       -14.5455                          -0 
-    13 point_low_4  B        11.6364            -0               
-    14 point_high_5 B       -8.72727                          -0 
-    15 point_low_5  B        17.4545            -0               
-    16 point_high_6 B       -5.81818                          -0 
-    17 point_low_6  B        20.3636            -0               
-    18 point_high_7 NU             0                          -0    -0.0909091 
-    19 point_low_7  B        26.1818            -0               
+     1 z            B            4.5                             
+     2 point_x_high_1
+                    B            -10                          -1 
+     3 point_x_low_1
+                    NL            -1            -1                         0.5 
+     4 point_x_high_2
+                    B            -10                          -2 
+     5 point_x_low_2
+                    B             -1            -2               
+     6 point_x_high_3
+                    B            -10                          -3 
+     7 point_x_low_3
+                    B             -1            -3               
+     8 point_x_high_4
+                    B            -10                          -5 
+     9 point_x_low_4
+                    B             -1            -5               
+    10 point_x_high_5
+                    B            -10                          -7 
+    11 point_x_low_5
+                    B             -1            -7               
+    12 point_x_high_6
+                    B            -10                          -8 
+    13 point_x_low_6
+                    B             -1            -8               
+    14 point_x_high_7
+                    NU           -10                         -10          -0.5 
+    15 point_x_low_7
+                    B             -1           -10               
+    16 point_high_1 B           -8.8                          -0 
+    17 point_low_1  B            0.2            -0               
+    18 point_high_2 B           -6.6                          -0 
+    19 point_low_2  B            2.4            -0               
+    20 point_high_3 B           -4.4                          -0 
+    21 point_low_3  B            4.6            -0               
+    22 point_high_4 NU             0                          -0         < eps
+    23 point_low_4  B              9            -0               
+    24 point_high_5 B           -1.1                          -0 
+    25 point_low_5  B            7.9            -0               
+    26 point_high_6 B           -4.4                          -0 
+    27 point_low_6  B            4.6            -0               
+    28 point_high_7 NU             0                          -0         < eps
+    29 point_low_7  B              9            -0               
 
    No. Column name  St   Activity     Lower bound   Upper bound    Marginal
 ------ ------------ -- ------------- ------------- ------------- -------------
-     1 a            B        2.90909             0               
-     2 b            NL             0             0                     1.45455 
-     3 c            B             16             0               
-     4 t            B        13.0909                             
+     1 a            B           -8.8                             
+     2 b            B            5.5                             
+     3 c            B             12                             
+     4 t            B            4.5                             
 
 Karush-Kuhn-Tucker optimality conditions:
 
-KKT.PE: max.abs.err = 3.55e-15 on row 6
-        max.rel.err = 6.54e-17 on row 5
+KKT.PE: max.abs.err = 1.42e-14 on row 28
+        max.rel.err = 1.48e-16 on row 3
         High quality
 
 KKT.PB: max.abs.err = 0.00e+00 on row 0
